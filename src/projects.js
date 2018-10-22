a62128b18f2af827c29632492d45fbe3ea717b8a
Torbjørn N. Høiland <t_hoiland@hotmail.com>
2018-10-22 23:07:57 +0200

    more stuff

--- src/projects.js
+++ src/projects.js
@@ -1,12 +1,18 @@
 let publicurl = process.env.PUBLIC_URL;
 publicurl = ".";
+import { org } from `./assets/orgmanager.jpg`;
+import { libdgx } from `./assets/libgdxgame.png`;
+import { handsy } from `./assets/projecthandsy.png`;
+import { photo } from `./assets/photoviewer.png`;
+
+
 export const projects = [{
     "title": "Organisation Unit Manager",
     "subtitle": "AngularJS",
     "status": "Done",
     "description": "This was a project in the 'open source software development' course @UiO. Here we used the DHIS2 web API to load and display organisation units",
     "icon": "",
-    "card_img": `${publicurl}/assets/orgmanager.jpg`,
+    "card_img": org,
     "location": "GitHub",
     "links": {
         "code": "https://github.com/simjes/dhis2-orgunit",
@@ -20,7 +26,7 @@ export const projects = [{
     "status": "Done",
     "description": "This was my bachelor thesis. A 2D game with simple controls, made using LibGDX and written in Java.",
     "icon": "",
-    "card_img": `${publicurl}/assets/libgdxgame.png`,
+    "card_img": libdgx,
     "location": "GitHub",
     "links": {
         "code": "https://github.com/Kjelli/AAGame/tree/deliverables",
@@ -34,7 +40,7 @@ export const projects = [{
     "status": "Done",
     "description": "This was a group assignment in 'UNIK4690 - Computer Vision' course at UiO via UNIK. Hand and gesture recognition using OpenCV, written in C++",
     "icon": "",
-    "card_img": `${publicurl}/assets/projecthandsy.png`,
+    "card_img": handsy,
     "location": "GitHub",
     "links": {
         "code": "https://github.com/Tobzor/UNIK4690",
@@ -48,7 +54,7 @@ export const projects = [{
     "status": "Done",
     "description": "This was an assignment in a software development course at University of Stavanger. Displays photos from a specified folder, written in C#",
     "icon": "",
-    "card_img": `${publicurl}/assets/photoviewer.png`,
+    "card_img": photo,
     "location": "BitBucket",
     "links": {
         "code": "https://bitbucket.org/gkverneland/uis.dat210.photomanager",
