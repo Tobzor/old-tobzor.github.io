e2eb819c35b39e7bb0aa3c9505325ecea29423fc
Torbjørn N. Høiland <t_hoiland@hotmail.com>
2018-10-22 22:51:05 +0200

    Testing relative paths

--- src/ClientApp/Components/Header.js
+++ src/ClientApp/Components/Header.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
 
-import profileimg from "../../assets/profile_img.png";
-
+const publicurl = process.env.PUBLIC_URL;
 class Header extends Component {
     render() {
         return (
             <div className="header">
-                <img src={profileimg} alt="profile" className="profile-image" />
+                <img src={`${publicurl}assets/profile_img.png`} alt="profile" className="profile-image" />
             </div>
         );
     }
