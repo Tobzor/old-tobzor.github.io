6e6354529e5af97786b2cddd10219ffbc9eb1c45
Torbjørn N. Høiland <t_hoiland@hotmail.com>
2018-10-28 22:45:52 +0100

    testing static server

--- src/projects.js
+++ src/projects.js
@@ -1,9 +1,8 @@
-// let publicurl = process.env.PUBLIC_URL;
-// import { org } from "./assets/orgmanager.jpg";
-// import { libdgx } from "./assets/libgdxgame.png";
-// import { handsy } from "./assets/projecthandsy.png";
-// import { photo } from "./assets/photoviewer.png";
-
+const org = "./assets/orgmanager.jpg";
+const libdgx = "./assets/libgdxgame.png";
+const handsy = "./assets/projecthandsy.png";
+const photo = "./assets/photoviewer.png";
+let publicurl = process.env.PUBLIC_URL;
 
 export const projects = [{
     "title": "Organisation Unit Manager",
@@ -11,7 +10,7 @@ export const projects = [{
     "status": "Done",
     "description": "This was a project in the 'open source software development' course @UiO. Here we used the DHIS2 web API to load and display organisation units",
     "icon": "",
-    "card_img": "org",
+    "card_img": publicurl + org,
     "location": "GitHub",
     "links": {
         "code": "https://github.com/simjes/dhis2-orgunit",
@@ -25,7 +24,7 @@ export const projects = [{
     "status": "Done",
     "description": "This was my bachelor thesis. A 2D game with simple controls, made using LibGDX and written in Java.",
     "icon": "",
-    "card_img": "libdgx",
+    "card_img": publicurl + libdgx,
     "location": "GitHub",
     "links": {
         "code": "https://github.com/Kjelli/AAGame/tree/deliverables",
@@ -39,7 +38,7 @@ export const projects = [{
     "status": "Done",
     "description": "This was a group assignment in 'UNIK4690 - Computer Vision' course at UiO via UNIK. Hand and gesture recognition using OpenCV, written in C++",
     "icon": "",
-    "card_img": "handsy",
+    "card_img": publicurl + handsy,
     "location": "GitHub",
     "links": {
         "code": "https://github.com/Tobzor/UNIK4690",
@@ -53,7 +52,7 @@ export const projects = [{
     "status": "Done",
     "description": "This was an assignment in a software development course at University of Stavanger. Displays photos from a specified folder, written in C#",
     "icon": "",
-    "card_img": "photo",
+    "card_img": publicurl + photo,
     "location": "BitBucket",
     "links": {
         "code": "https://bitbucket.org/gkverneland/uis.dat210.photomanager",
